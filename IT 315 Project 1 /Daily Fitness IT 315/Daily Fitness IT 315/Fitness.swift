28704ebb47d0125cc02034992d06941c9e7a103d
STech165 <144859418+STech165@users.noreply.github.com>
2023-12-03 11:04:29 -0500

    Add files via upload

---
+++ IT 315 Project 1 /Daily Fitness IT 315/Daily Fitness IT 315/Fitness.swift
@@ -0,0 +1,20 @@
+//
+//  Fitness.swift
+//  Daily Fitness IT 315
+//
+//  Created by Suguna Kurma on 10/15/23.
+//
+import UIKit
+import Foundation
+
+class Fitness {
+    var FoodName = ""
+    var FoodDescription = ""
+    var FoodTime = ""
+    var FoodBenefits = ""
+    var FoodIngrediants = ""
+    var FoodSite = ""
+    var FoodImage = ""
+    var NameTitle = ""
+    var Advisory = ""
+}
